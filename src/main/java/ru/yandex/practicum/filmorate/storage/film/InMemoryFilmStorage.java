218166db1c12ddf79c0a30ebbdb0140ba6662aee
Волынка <olganv1707@yandex.ru>
2023-05-18 23:11:39 +0300

    feat:поправила по комментам ревью

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -17,7 +17,6 @@ import java.util.Map;
 @Getter
 public class InMemoryFilmStorage implements FilmStorage {
     private final Map<Long, Film> films = new HashMap<>();
-    private static final LocalDate MIN_DATA = LocalDate.of(1895, 12, 28);
     private long id = 0;
 
     @Override
