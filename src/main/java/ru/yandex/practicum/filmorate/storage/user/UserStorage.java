d2a94ef0598a042df4294af0ff960f8895cbf96b
Волынка <olganv1707@yandex.ru>
2023-05-30 00:44:28 +0300

    feat:поправила codestyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.user;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserStorage {
 
@@ -16,6 +15,7 @@ public interface UserStorage {
     User create(User user);
 
     User updateUser(User user);
+    
     User getUserById(Long id);
 
 }
